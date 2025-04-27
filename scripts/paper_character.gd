5970cd0946c00cf13c5c583f1585097a9b527d4e
LeCrazyChkn <aimanakmalhakim@gmail.com>
2025-04-27 16:28:51 +0800

    Remove Camera Rotate
    The camera rotation is messing with the rest of the project so far. I have decided to remove it to keep things simple(I can just design the world around a fixed camera layout).

--- scripts/paper_character.gd
+++ scripts/paper_character.gd
@@ -13,19 +13,19 @@ var face_up: bool = false #Vertical
 var target_velocity : Vector3 = Vector3.ZERO
 
 #Cmaera Rotate Variables (ChatGPT)
-@export var follow_speed : float = 5.0
-@export var rotation_speed : float = 5.0
-@export var player_path : NodePath
+#@export var follow_speed : float = 5.0
+#@export var rotation_speed : float = 5.0
+#@export var player_path : NodePath
 
 var player : Node3D
 var target_rotation_y : float = 0.0
 
 	
-
-func _ready() -> void:
-	sprite.play("down_idle")
-	player = get_node(player_path)
-	target_rotation_y = rotation.y
+#ChatGPT camera rotation
+#func _ready() -> void:
+	#sprite.play("down_idle")
+	#player = get_node(player_path)
+	#target_rotation_y = rotation.y
 	
 
 func _physics_process(delta):
@@ -120,18 +120,18 @@ func _physics_process(delta):
 
 
 #Rotation function (ChatGPT)
-func _process(delta):
-	# --- Smoothly rotate toward target_rotation_y ---
-	var current_rot = rotation
-	current_rot.y = lerp_angle(current_rot.y, target_rotation_y, rotation_speed * delta)
-	rotation = current_rot
-
-	# --- Check for input to rotate ---
-	if Input.is_action_just_pressed("camera_rotate_left"):
-		target_rotation_y += deg_to_rad(90)
-	elif Input.is_action_just_pressed("camera_rotate_right"):
-		target_rotation_y -= deg_to_rad(90)
-
-	#debugging: see CameraRoot direction
-	print(target_rotation_y)
+#func _process(delta):
+	## --- Smoothly rotate toward target_rotation_y ---
+	#var current_rot = rotation
+	#current_rot.y = lerp_angle(current_rot.y, target_rotation_y, rotation_speed * delta)
+	#rotation = current_rot
+#
+	## --- Check for input to rotate ---
+	#if Input.is_action_just_pressed("camera_rotate_left"):
+		#target_rotation_y += deg_to_rad(90)
+	#elif Input.is_action_just_pressed("camera_rotate_right"):
+		#target_rotation_y -= deg_to_rad(90)
+#
+	##debugging: see CameraRoot direction
+	#print(target_rotation_y)
 	
